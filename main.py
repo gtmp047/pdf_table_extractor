d1beddca565cf0ab801678802844e0a72a2368d3
Anton O. Burygin <a.burygin@norsi-trans.ru>
2021-11-08 22:41:55 +0300

    v_1

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 from imutils import contours
 
+import pytesseract
+
+
+# Adding custom options
+TESSERACT_CONF = r'--oem 3 --psm 6'
+
+
+
+
 from classes import Cell
 
 TRESHOLD = 0.9
@@ -75,29 +84,63 @@ def get_detailed_cell_info(file_path):
     return min_area_cnts
 
 
+def check_region_is_table(region, dots):
+    x, y, w, h = cv2.boundingRect(region)
+    dots_region = dots[y:y+h, x:x+w]
+    if not cv2.countNonZero(dots_region):
+        return False
+    return True
+
+
+def extract_text_from_region(region):
+    mask = np.ones(image.shape, dtype=np.uint8) * 255
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+
+    cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+    for c in cnts:
+        area = cv2.contourArea(c)
+        if area < 1000:
+            x, y, w, h = cv2.boundingRect(c)
+            mask[y:y + h, x:x + w] = image[y:y + h, x:x + w]
+
+    text = pytesseract.image_to_string(thresh, lang='rus', config=TESSERACT_CONF)
+    return text
+
 if __name__ == '__main__':
     file_path = 'pdf_image_data/1.jpg'
+    main_image = cv2.imread(file_path)
+
     detailed_data = get_detailed_cell_info(file_path)
     region_blocks = get_cell_blocks(file_path)
+    _, dots = detect_table(main_image)
 
     for region_block in region_blocks:
         region_block_rect = cv2.boundingRect(region_block)
 
+        if not check_region_is_table(region_block, dots):
+            # определяем просто как текст
+            x, y, w, h = cv2.boundingRect(region_block)
+            text1 = extract_text_from_region(main_image[y:y + h, x:x + w])
+            text2 = pytesseract.image_to_string(main_image[y:y + h, x:x + w], lang='rus', config=TESSERACT_CONF)
+            pass
+        else:
+            # выделяем блоки в таблице
 
-        detailed_data_cell_in_region = []
-        for detailed_data_cell in detailed_data:
-            detailed_data_cell_rect = cv2.boundingRect(detailed_data_cell)
+            detailed_data_cell_in_region = []
+            for detailed_data_cell in detailed_data:
+                detailed_data_cell_rect = cv2.boundingRect(detailed_data_cell)
+                simplisity = Cell.interception_perc(Cell(*region_block_rect), Cell(*detailed_data_cell_rect))
+                if simplisity >= TRESHOLD and region_block_rect != detailed_data_cell_rect:
+                    detailed_data_cell_in_region.append(detailed_data_cell)
 
-            simplisity = Cell.interception_perc(Cell(*region_block_rect), Cell(*detailed_data_cell_rect))
-            if simplisity >= TRESHOLD and region_block_rect != detailed_data_cell_rect:
-                detailed_data_cell_in_region.append(detailed_data_cell)
+        # image = cv2.imread(file_path)
+        # table_image = None
+        # for c in detailed_data_cell_in_region:
+        #     x, y, w, h = cv2.boundingRect(c)
+        #     table_image = cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 1)
+        # plt.imshow(table_image)
+        # plt.show()
+        # cv2.namedWindow('detecttable', cv2.WINDOW_NORMAL)
 
-        image = cv2.imread(file_path)
-        table_image = None
-        for c in detailed_data_cell_in_region:
-            x, y, w, h = cv2.boundingRect(c)
-            table_image = cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 1)
-        plt.imshow(table_image)
-        plt.show()
-        cv2.namedWindow('detecttable', cv2.WINDOW_NORMAL)
-        # break
